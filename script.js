28e1fd37e2075c68d1cc9ebbc3a7e5dbc997e12a
Heracitta <heriyaahmedsiraje@gmail.com>
2026-06-25 12:43:02 +0300

    final version added

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-
-document.getElementById('generate').addEventListener('click',()=>{
+let generate = document.getElementById('generate')
+    generate.addEventListener('click',()=>{
     let input = document.getElementById('input')
     let url = `https://quickchart.io/qr?text=${encodeURIComponent(input.value)}`
-    let qrcode = document.getElementById('qrcode')
+    let qrcode = document.getElementById('qrcode');
+
 
     if(input.value == ''){
         alert('please enter value before generating')
         return
     }
     else{
-        qrcode.src= url
+            generate.innerHTML = 'Generating'
+                setTimeout(()=>{
+                  generate.innerHTML = 'generated'
+                  document.getElementById('section').style.height = '450px'
+                   document.getElementById('box').style.display = 'block'
+                   qrcode.src= url  
+             },1000)
+                  
+        
     }
+
 })
